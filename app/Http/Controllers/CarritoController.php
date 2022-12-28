f8ab3a8d67234c034df05492d21c85a63d872f38
stevins23 <stevinsgonzalez23@gmail.com>
2022-12-28 17:44:30 +0000

    Modelos, controladores, migraciones, etc creados

---
+++ app/Http/Controllers/CarritoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCarritoRequest;
+use App\Http\Requests\UpdateCarritoRequest;
+use App\Models\Carrito;
+
+class CarritoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCarritoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCarritoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Carrito  $carrito
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Carrito $carrito)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Carrito  $carrito
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Carrito $carrito)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCarritoRequest  $request
+     * @param  \App\Models\Carrito  $carrito
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCarritoRequest $request, Carrito $carrito)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Carrito  $carrito
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Carrito $carrito)
+    {
+        //
+    }
+}
